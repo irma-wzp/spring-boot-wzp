fbd214f98b66dbbc6391c32c121b9f27ff74a202
irma-wzp <528266974@qq.com>
2018-01-09 14:42:20 +0800

    Initial commit

---
+++ src/main/java/com/lingc/SpringBootWzpApplication.java
@@ -0,0 +1,12 @@
+package com.lingc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWzpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWzpApplication.class, args);
+	}
+}
